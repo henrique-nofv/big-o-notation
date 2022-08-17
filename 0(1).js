b980d9e2a8d8b52aada35097f1433313a50a30b6
Henrique Noronha Favorette <henrique.nofv@gmail.com>
2022-08-16 23:31:08 -0300

    Create 0(1).js

---
+++ 0(1).js
@@ -0,0 +1,11 @@
+/*
+  O(1) -> Linear time
+  O número de operações não altera conforme o crescimento dos inputs.
+*/
+const persons = new Array(10500000).fill('Henrique', 0);
+
+const firstItemInArray = (array) => {
+  return array[0];
+}
+
+console.log(firstItemInArray(persons))
